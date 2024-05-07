b2066e00c6d30380c1d9500df0919d33d9c507c0
Aman Rawat <rawataman101@gmail.com>
2024-05-07 12:11:14 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import DateTimeDiv from "./DateTimeDiv";
+import "./App.css";
 function App() {
+  const [toggle, setToggle] = useState(false);
+  const [dateTime, setDateTime] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //get data-time from form using FormData
+    const form = e.target; // get form element
+    const formData = new FormData(form); //formdata object
+    const datetimeValue = formData.get("datetime-input"); // get value of datetime input
+    setDateTime(new Date(datetimeValue).getTime());
+    dateTime && setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    if (!toggle) {
+      return;
+    }
+
+    const timer = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = dateTime - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(timer);
+        setToggle(false);
+        setMessage("The countdown is over! What's your next advednture?");
+      } else if (days > 100) {
+        clearInterval(timer);
+        setToggle(false);
+        setMessage("Selected time is more than 100 days");
+      } else {
+        setTimeLeft({
+          days: `${days}d`,
+          hours: `${hours}h`,
+          minutes: `${minutes}m`,
+          seconds: `${seconds}s`,
+        });
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [toggle, dateTime]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Countdown <span className="timerColor">Timer</span>
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="dateTimeInput"
+          type="datetime-local"
+          name="datetime-input"
+          id="datetime-input"
+          required
+        />
+        <br />
+        <button className="toggleButton">
+          {toggle ? "Cancel Timer" : "Start Timer"}
+        </button>
+      </form>
+      <div className="container">
+        {!message ? (
+          <>
+            <DateTimeDiv data={timeLeft?.days} />
+            <DateTimeDiv data={timeLeft?.hours} />
+            <DateTimeDiv data={timeLeft?.minutes} />
+            <DateTimeDiv data={timeLeft?.seconds} />
+          </>
+        ) : (
+          <h3>{message}</h3>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/DateTimeDiv.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./DateTimeDiv.css";
+
+function DateTimeDiv({ data }) {
+  return <div className="wrapper">{data}</div>;
+}
+
+export default DateTimeDiv;
